6a83aea3775416148d47c74fa15f08472e481ec5
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-08-24 20:06:06 -0300

    Exercicio 4 de alteracao aleatoria corrigido e com 100%

--- TP/TP01/Q04/Q04.java
+++ TP/TP01/Q04/Q04.java
@@ -1,42 +1,42 @@
 import java.util.Random;
 class Q04 {
     public static void main(String[] args){
-        //Variaveis
         String input;
+
+        //Gerador de numero aleatorio com seed setada em 4
         Random gerador = new Random();
         gerador.setSeed(4);
         
+        
         while(!(input = leInput()).equals("FIM")){
-            String aux = trocaString(input);
+            char a = (char)('a' + Math.abs(gerador.nextInt() % 26));//Primeira ocorrencia
+            char b = (char)('a' + Math.abs(gerador.nextInt() % 26));//Segunda ocorrencia
+            String aux = trocaString(input, a, b);
             imprimir(aux);
         }
         
     }
 
-    public static String leInput(){
+    public static String leInput(){//Metodo para ler do teclado (fiz atoa)
         return MyIO.readLine();
     }
     
-    public static String trocaString(String temp){
-        //Variaveis do metodo
-        String aux = "";
-        char[] newChar = new char[temp.length()];
-        //Variaveis e set para utilizar a classe random
-        
-        int a = (char)'a' + Math.abs(gerador.nextInt() % 26);
-        int b = 'a' + Math.abs(gerador.nextInt() % 26);
+    //Metodo para trocar a primeira ocorrencia pela segunda
+    public static String trocaString(String texto, char a, char b){
+        String newString = "";
 
-        for(int i = 0; i < temp.length(); i++){
-            newChar[i] = ( char ) ( temp.charAt(i) );
-            if(newChar[i] == (char)a){
-                newChar[i] = (char)b;
+        for(int i = 0; i < texto.length(); i++){
+            //Caso a primeira ocorrencia seja validade, troca pela segunda ocorrencia
+            if(texto.charAt(i) == a){ 
+                newString += b;
+            } else {
+                newString += texto.charAt(i);
             }
-            aux += newChar[i];
         }
-        return aux;
+        return newString;
     }
 
-    public static void imprimir(String out){
+    public static void imprimir(String out){ //Metodo para imprimir
         MyIO.println(out);
     }
     
